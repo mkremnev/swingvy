06d4ffc0927c242c1802048bb6bdc758fdc79f3c
Maxim Kremnev <m.kremnev@netlinux.ru>
2021-05-13 14:36:46 +0300

    Setup sass

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
 };
